0153c88cd9a2c309737342372eec348464469d07
Robert <Robert@bobbys-mbp.mynetworksettings.com>
2023-11-12 22:20:27 -0500

    running version of LSTM seq2seq

--- seq2seqLSTM/seq2seqLSTM.py
+++ seq2seqLSTM/seq2seqLSTM.py
@@ -16,8 +16,21 @@ from torch.utils.tensorboard import SummaryWriter
 from nltk.tokenize import word_tokenize
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+# from utils import translate_sentence, bleu
 
-### Preprocessing for seq2seq
+### Helper functions
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+### Preprocessing for seq2seq ###
 
 nrows = 1000
 df = pd.read_csv("../../data/en-fr.csv", nrows=nrows)
@@ -51,13 +64,149 @@ train_data, test_data = TabularDataset.splits(
 english.build_vocab(train_data, max_size=10000, min_freq=2)
 french.build_vocab(train_data, max_size=10000, min_freq=2)
 
+### Define the Architecture ###
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, p):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+
+    def forward(self, x):
+        # x shape: (seq_length, N)
+        embedding = self.dropout(self.embedding(x))
+        # embedding shape: (seq_length, N, embedding_size)
+        outputs, (hidden, cell) = self.rnn(embedding)
+        return hidden, cell
+    
+class Decoder(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, output_size, num_layers, p):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden, cell):
+        # shape of x: (N) but we want (1, N)
+        x = x.unsqueeze(0)
+
+        embedding = self.dropout(self.embedding(x))
+        # embedding shape: (1, N, embedding_size)
+
+        outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
+        # shape of outputs: (1, N, hidden_size)
+
+        predictions = self.fc(outputs)
+        # shape of predictions: (1, N, length_of_vocab)
+        predictions = predictions.squeeze(0)
+        return predictions, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, source, target, teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(french.vocab)
+
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
+
+        hidden, cell = self.encoder(source)
+        # Grab start token
+        x = target[0]
+
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+            outputs[t] = output
+            best_guess = output.argmax(1)
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+
+        return outputs
+
+
+### Training the Model ###
+
+# Training hyperparameters
+num_epochs = 10
+learning_rate = 0.001
+batch_size = 32
+
+# Model hyperparameters
+load_model = False
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+input_size_encoder = len(english.vocab)
+input_size_decoder = len(french.vocab)
+output_size = len(french.vocab)
+encoder_embedding_size = 200
+decoder_embedding_size = 200
+hidden_size = 1024
+num_layers = 2
+enc_dropout = 0.5
+dec_dropout = 0.5
+
+
+# Tensorboard
+writer = SummaryWriter(f'runs/loss_plot')
+step = 0
+
 train_iterator, test_iterator = BucketIterator.splits(
     (train_data, test_data),
-    batch_size=64,
-    device='mps')
+    batch_size=batch_size,
+    sort_within_batch = True,
+    sort_key = lambda x: len(x.English),
+    device=device)
 
-for batch in train_iterator:
-    print(batch)
+encoder_net = Encoder(input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout).to(device)
+decoder_net = Decoder(input_size_decoder, decoder_embedding_size, hidden_size, output_size, num_layers, dec_dropout).to(device)
+
+model = Seq2Seq(encoder_net, decoder_net).to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+pad_idx = french.vocab.stoi['<pad>']
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+
+if load_model:
+    load_checkpoint(torch.load('my_checkpoint.pth.ptar'), model, optimizer)
+
+training_losses = []
+for epoch in range(1, num_epochs+1):
+
+    checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    save_checkpoint(checkpoint)
+
+    # for batch_idx, batch in enumerate(train_iterator):
+    for batch in tqdm(train_iterator, desc=f'Train Epoch: {epoch}/{num_epochs}'):
+        inp_data = batch.English.to(device)
+        target = batch.French.to(device)
+        output = model(inp_data, target)
+        # output shape: (trg_len, batch_size, output_dim)
+        output = output[1:].reshape(-1, output.shape[2])
+        target = target[1:].reshape(-1)
+
+        optimizer.zero_grad()
+        loss = criterion(output, target)
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=1)
+        optimizer.step()
+
+        writer.add_scalar('Training Loss', loss, global_step=step)
+        step += 1
+
+
+
+
 
 
 
