39bfb8b8697d1a545ba593a892afa20c452bf4f4
mdontas <mdontas@github.com>
2023-11-15 16:07:36 -0500

    Train word embeddings using pytorch vocabs

--- trainEmbeddinsEn.py
+++ trainEmbeddinsEn.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from gensim.models import Word2Vec
 import os
+import torch
 
 class MySentences(object):
     def __init__(self, dirname):
@@ -13,13 +14,15 @@ class MySentences(object):
                 yield line.split()
 
 # Create vocab map
-vocab = pd.read_csv("vocabEn.csv")
-vocab = vocab[:20000] # keep the 20k most frequent words
-vocab_map = vocab.set_index('en').to_dict()['count']
+# vocab = pd.read_csv("vocabEn.csv")
+# vocab = vocab[:20000] # keep the 20k most frequent words
+# vocab_map = vocab.set_index('en').to_dict()['count']
+vocab = torch.load("en_vocab.pth").get_itos()
+
 
 model = Word2Vec(vector_size=100, window=5, workers=4, negative=2, sg=1)
-model.build_vocab_from_freq(vocab_map, keep_raw_vocab=True)
+model.build_vocab(vocab)
 # model = Word2Vec.load("word2vec_0.model")
 
-model.train(MySentences("sentencesEn"), total_words=len(vocab_map), epochs=20)
+model.train(MySentences("sentencesEn"), total_words=len(vocab), epochs=20)
 model.save("embeddingsEn.model")
--- trainEmbeddinsFr.py
+++ trainEmbeddinsFr.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from gensim.models import Word2Vec
 import os
+import torch
 
 class MySentences(object):
     def __init__(self, dirname):
@@ -13,12 +14,14 @@ class MySentences(object):
                 yield line.split()
 
 # Create vocab map
-vocab = pd.read_csv("vocabFr.csv")
-vocab = vocab[:20000] # keep the 20k most frequent words
-vocab_map = vocab.set_index('fr').to_dict()['count']
+# vocab = pd.read_csv("vocabFr.csv")
+# vocab = vocab[:20000] # keep the 20k most frequent words
+# vocab_map = vocab.set_index('fr').to_dict()['count']
+vocab = torch.load("fr_vocab.pth").get_itos()
+
 
 model = Word2Vec(vector_size=100, window=5, workers=4)
-model.build_vocab_from_freq(vocab_map, keep_raw_vocab=True)
+model.build_vocab(vocab)
 
-model.train(MySentences("sentencesFr"), total_words=len(vocab_map), epochs=20)
+model.train(MySentences("sentencesFr"), total_words=len(vocab), epochs=20)
 model.save("embeddingsFr.model")
